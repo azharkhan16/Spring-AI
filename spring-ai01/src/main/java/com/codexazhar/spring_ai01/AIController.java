5ea31f52abd430b18d0af5b4a10980ceba0892f0
azharkhan16 <azharkhan75447355@gmail.com>
2026-06-01 23:37:33 +0530

    Updated sports-v2 endpoint with SystemMessage and UserMessage support

--- spring-ai01/src/main/java/com/codexazhar/spring_ai01/AIController.java
+++ spring-ai01/src/main/java/com/codexazhar/spring_ai01/AIController.java
@@ -1,6 +1,7 @@
 package com.codexazhar.spring_ai01;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -26,7 +28,7 @@ public class AIController {
                 .user(message)
                 .call()
                 .content();
-        //        System.out.println(response);
+//                System.out.println(response);
         return response;
     }
 
@@ -52,14 +54,19 @@ public class AIController {
     @GetMapping("/sports-v2")
     public String findPopularSportsPersonTwo(@RequestParam String sports) {
 
+        var systemMessage = new SystemMessage("""
+                Your primary role is to provide information about popular sports personalities.
+                If someone asks about anything else, you can say "I am sorry, but I focus on sharing information
+                about sports personalities only. If you have any question related to sport, feel free to ask!."."""
+        );
         var userMessage = new UserMessage(
                 String.format("""
                         List of 5 most popular personalities in %s
                         along with their Career Achievements.
                         Show the details in proper Readable format.""", sports)
         );
-        Prompt prompt = new Prompt(userMessage);
 
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
         return this.chatClient.prompt(prompt)
                 .call()
                 .content();
